2c718ea33f002b070edb4b0064c15440b702d82f
john makateto <jmakateto@gmail.com>
2023-08-28 13:09:58 +0300

    Fixed print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  # Print an empty list
+        return
+    elif length == 1:
+        print([0])  # Print [0] for length 1
+        return
+    
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+    print(fib_sequence)
